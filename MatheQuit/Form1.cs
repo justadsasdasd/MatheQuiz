49b604437b8ba2b73f7f3a0134df6a72eb93c61b
justadsasdasd <114064554+justadsasdasd@users.noreply.github.com>
2022-09-27 08:59:42 +0200

    Add files via upload

---
+++ MatheQuit/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mathe_Quiz
+{
+    public partial class MatheQuiz : Form
+    {
+        // Create a Random object called randomizer 
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+        // Create a Random object called randomizer 
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+
+
+
+
+
+
+        public MatheQuiz()
+        {
+        InitializeComponent();
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        }
+    }
